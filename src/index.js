4ce514ea9bcdf6dce9643f9b97cdb16413c45189
Muhammad Nurudin <muhammadnurudin01@gmail.com>
2021-11-06 12:43:48 +0700

    fix routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './pages/App';
+import Routers from './routes';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routers />
+    </BrowserRouter>
+  </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
