fe43fb01dc8c825e563f641f5a1570d6a1097a73
lisapriliani <lisaapriliani4@gmail.com>
2021-11-16 18:28:12 +0700

    g

--- src/pages/Userpage.js
+++ src/pages/Userpage.js
@@ -23,17 +23,45 @@ export default function Userpage() {
       </div>
 
       <section>
-        <div className="container mb-5" />
-        <div className="row mt-1 gx-0 gy-4 gx-md-4 mx-2" />
-        <div className="col-xxl-4 col-xl-4 col-lg-4 g-5 col-md-4 col-sm-4 col-12 m-auto mt-4" />
-        <Card
-          src="https://images.unsplash.com/photo-1512915922686-57c11dde9b6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=873&q=80"
-          nama="Townhouse Bali"
-          lokasi="Jimbaran, Badung"
-          agen="Home Agency"
-          harga="IDR 600M"
-          tipe="House"
-        />
+        <div className="container py-5 mt-5">
+          <div className="row mt-1 gx-0 gy-4 gx-md-4">
+            <div className="col-md-6 col-xl-4">
+              <Card
+                src="https://images.unsplash.com/photo-1512915922686-57c11dde9b6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+                top="Rumah"
+                nama="Townhouse Bagus 2 Lantai"
+                lokasi="Jimbaran, Badung"
+                harga="IDR. 600 M"
+                agen="Futurehomy Agency"
+                href="/searchProperty/1"
+              />
+            </div>
+
+            <div className="col-md-6 col-xl-4">
+              <Card
+                src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+                top="Rumah"
+                nama="Townhouse Bagus 2 Lantai"
+                lokasi="Jimbaran, Badung"
+                harga="IDR. 600 M"
+                agen="Futurehomy Agency"
+                href="/searchProperty/1"
+              />
+            </div>
+
+            <div className="col-md-6 col-xl-4">
+              <Card
+                src="https://images.unsplash.com/photo-1627141234469-24711efb373c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+                top="Rumah"
+                nama="Townhouse Bagus 2 Lantai"
+                lokasi="Jimbaran, Badung"
+                harga="IDR. 600 M"
+                agen="Futurehomy Agency"
+                href="/searchProperty/1"
+              />
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
