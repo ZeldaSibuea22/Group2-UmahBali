bd2c28a8f670afe8d2765f9fca4828c6678645a8
Muhammad Nurudin <muhammadnurudin01@gmail.com>
2021-11-17 19:06:25 +0700

    test

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,17 +6,20 @@ export default function NotFound() {
   return (
     <Layout>
       <section className="container pb-5 px-5">
-        <div className="row">
-          <div className="col-12 col-sm-9 col-md-6">
+        <div className="row ms-auto me-auto">
+          <div className="col-12 col-sm-9 col-md-6 ms-auto me-auto">
             <img src="https://user-images.githubusercontent.com/61933958/142138061-1f764917-c60d-4d0d-b247-842b4e12e94d.png" alt="Not Found" className="img-fluid" />
             </div>        
           <div className="col-12">
-              <h4>Ooops... Halaman yang anda tuju tidak tersedia.</h4>
-              <p className="text-dark mb-2 mt-3">Apakah anda yakin URL yang anda tuju sudah benar?</p>
-              <p className="text-dark mb-2">Mungkin halaman yang anda tuju sudah dihapus atau dipindahkan.</p>
+              <h4 className="text-center">Ooops... Halaman yang anda tuju tidak tersedia.</h4>
+              <p className="text-dark text-center mb-2 mt-3">Apakah anda yakin URL yang anda tuju sudah benar?</p>
+              <p className="text-dark text-center mb-2">Mungkin halaman yang anda tuju sudah dihapus atau dipindahkan.</p>
             
             <Link to="/">
-              <button className="btn btn-sm btn-outline-main mt-5">Kembali ke Beranda</button>
+              <div className="d-flex">
+                <button className="btn btn-sm btn-outline-main mt-5 ms-auto me-auto text-decoration-none">Kembali ke Beranda</button>
+              </div>
+              
             </Link>
           </div>
         </div>
