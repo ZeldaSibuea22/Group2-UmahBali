6bc831e180821603450c35d829b1b3e136184a87
ZeldaSibuea22 <zelda.sibuea43@gmail.com>
2021-11-17 18:37:56 +0700

    update-properties

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,12 @@ import Agents from "../pages/Agents";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import DetailProperty from "../pages/Detail";
+
 import Properties_Search from "../pages/Properties_Search";
+
+import Userpage from "../pages/Userpage";
+import NotFound from "../pages/NotFound";
+
 
 export default function Routers() {
   return (
@@ -16,6 +21,8 @@ export default function Routers() {
       <Route path="/SignUp" element={<SignUp />}></Route>
       <Route path="/SignIn" element={<SignIn />}></Route>
 
+      <Route path="/Userpage" element={<Userpage />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
 
     </Routes>
   );
